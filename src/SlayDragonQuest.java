512095dadc9864ea677adeffe618784f4c8bd98e
mx <403481397@qq.com>
2019-08-01 10:29:31 +0800

    Initial commit

---
+++ src/SlayDragonQuest.java
@@ -0,0 +1,11 @@
+public class SlayDragonQuest implements Quest{
+
+    public SlayDragonQuest(){
+
+    }
+
+    @Override
+    public void embark() {
+        System.out.println("砍龙任务");
+    }
+}
---
+++ src/mark.java
@@ -0,0 +1,10 @@
+public class mark {
+
+    public void beforeknight(){
+        System.out.println("任务要开始了");
+    }
+
+    public void afterknight(){
+        System.out.println("任务结束了");
+    }
+}
