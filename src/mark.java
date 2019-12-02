3927923d95361be2e3ab8b75478346581dade73b
jinjiaxin <1779778350@qq.com>
2019-12-02 18:03:16 +0800

    123456

--- src/mark.java
+++ src/mark.java
@@ -1,4 +1,4 @@
-public class mark {
+﻿public class mark {
 
     public void beforeknight(){
         System.out.println("任务要开始了");
@@ -6,5 +6,6 @@ public class mark {
 
     public void afterknight(){
         System.out.println("任务结束了");
+        System.out.println("zxc");
     }
 }
